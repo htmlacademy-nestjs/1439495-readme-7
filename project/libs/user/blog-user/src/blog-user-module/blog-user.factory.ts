4ed603072f6c6634b675cfc01739809b4573895b
Anna <anna.mironova89@yandex.ru>
2024-07-17 17:08:15 +0300

    Добавила репозиторий

--- project/libs/user/blog-user/src/blog-user-module/blog-user.factory.ts
+++ project/libs/user/blog-user/src/blog-user-module/blog-user.factory.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { User, EntityFactory } from '@project/shared-core';
 import { BlogUserEntity } from './blog-user.entity';
 
+@Injectable()
 export class BlogUserFactory implements EntityFactory<BlogUserEntity> {
   public create(entityPlainData: User): BlogUserEntity {
     return new BlogUserEntity(entityPlainData);

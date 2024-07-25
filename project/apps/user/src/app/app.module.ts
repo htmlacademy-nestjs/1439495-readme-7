5f3fdd146671073c112e1bfcd26967c134f18347
Anna <anna.mironova89@yandex.ru>
2024-07-25 15:54:55 +0300

    Добавила модуль для работы с конфигурацией

--- project/apps/user/src/app/app.module.ts
+++ project/apps/user/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BlogUserModule } from '@project/blog-user';
-import { AuthenticationModule } from '@project/authentication'
+import { AuthenticationModule } from '@project/authentication';
+import { UserConfigModule } from '@project/config';
 
 @Module({
   imports: [
     BlogUserModule,
-    AuthenticationModule
+    AuthenticationModule,
+    UserConfigModule
   ],
   controllers: [],
   providers: [],

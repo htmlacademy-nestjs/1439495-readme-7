6edd54ecf54085ca3683a23852bbd1f44f313f7b
Anna <anna.mironova89@yandex.ru>
2024-08-07 12:03:12 +0300

    Добавила валидацию для сервиса user

--- project/libs/user/authentication/src/dto/login-user.dto.ts
+++ project/libs/user/authentication/src/dto/login-user.dto.ts
@@ -1,15 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
 
 export class LoginUserDto {
   @ApiProperty({
     description: 'User email',
     example: 'ivanov@mail.ru'
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'User password',
     example: 'qwerty123'
   })
+  @IsString()
+  @MinLength(6, { message: AuthenticationValidateMessage.PasswordMinLength })
+  @MaxLength(12, { message: AuthenticationValidateMessage.PasswordMaxLength })
   public password: string;
 }
